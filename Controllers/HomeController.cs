cb57d10846eac50cc2882ec746d0efd4b85fe503
THUNWA007 <Vathunwa@gmail.com>
2019-08-30 21:44:43 +0700

    8/30/2019

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using netcore.Models;
+
+namespace netcore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+           
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+       // [Route("Home/Detail/{name}")]
+        public IActionResult Detail (string name,string lastname,string idstudent,string supername)
+       {
+            ViewBag.name = name;
+            ViewBag.lastname = lastname;
+            ViewBag.idstudent = idstudent;
+            ViewBag.supername = supername;
+        
+
+            return View();
+        }
+
+        
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        
+    }
+    
+}
---
+++ Models/page1.cs
@@ -0,0 +1,8 @@
+namespace netcore.Models
+{
+    public class page1
+    {
+        public string ID { get; set; }
+        public string NAME { get; set; }
+    }
+}
